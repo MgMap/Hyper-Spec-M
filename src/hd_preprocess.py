dfddc340e4b79eaadcf5a9a57823c6dc7b8bad9e
MgMap <minaungpaing.pp@gmail.com>
2025-02-02 16:57:51 -0800

    filter

--- src/hd_preprocess.py
+++ src/hd_preprocess.py
@@ -565,8 +565,8 @@ def load_process_single(
 ):
     #mgf mzml mzxml
     if no_limitations:
-        min_peaks = 1
-        min_mz_range = 0.01
+        min_peaks = 0
+        min_mz_range = 0.0
         mz_min = None
         mz_max = None
         remove_precursor_tolerance = 0.0
